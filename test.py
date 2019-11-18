7d01b45454f03e198248dd0357753c07c29edb55
yashkej1 <yashkejriwal23@gmail.com>
2019-11-19 00:47:15 +0530

    branch

--- test.py
+++ test.py
@@ -1,10 +1,18 @@
 <<<<<<< test.py
-a=0
+<<<<<<< test.py
+a=0 
 print(a)
+print(c)
 =======
  def sub(self,a,b):
 	self.a = a
 	self.b = b
 	return a-b
-	
 >>>>>>> sub.py
+	
+=======
+def add(a,b):
+	return(a+b)
+>>>>>>> add.py
+
+
